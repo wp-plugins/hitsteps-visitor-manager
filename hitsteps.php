427666876698118ec8041335970ab2db35e53f70
hitsteps <hitsteps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-12 09:14:07 +0000

    *chat button fix in main dashboard page
    git-svn-id: https://plugins.svn.wordpress.org/hitsteps-visitor-manager/trunk@856221 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitsteps.php
+++ hitsteps.php
@@ -4,7 +4,7 @@ Plugin Name: Hitsteps Visitor Manager
 Plugin URI: http://www.hitsteps.com/
 Description: Hitsteps is a powerful real time website visitor manager, it allow you to view and interact with your visitors in real time.
 Author: hitsteps.com
-Version: 1.38
+Version: 1.39
 Author URI: http://www.hitsteps.com/
 */ 
  
@@ -16,7 +16,6 @@ add_action('wp_head', 'hitsteps');
 
 
 function hitsteps(){
-
 global $_SERVER,$_COOKIE,$hitsteps_tracker;
 
 $option=get_hst_conf();
