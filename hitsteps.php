9cfc09551fe77f005a31c00f5e7d92cd1a537f57
hitsteps <hitsteps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-19 06:06:55 +0000

    * fixed few bugs related to chat service users on Firefox browser
    git-svn-id: https://plugins.svn.wordpress.org/hitsteps-visitor-manager/trunk@968080 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitsteps.php
+++ hitsteps.php
@@ -4,7 +4,7 @@ Plugin Name: Hitsteps Visitor Manager
 Plugin URI: http://www.hitsteps.com/
 Description: Hitsteps is a powerful real time website visitor manager, it allow you to view and interact with your visitors in real time.
 Author: hitsteps.com
-Version: 2.14
+Version: 2.15
 Author URI: http://www.hitsteps.com/
 */ 
 
