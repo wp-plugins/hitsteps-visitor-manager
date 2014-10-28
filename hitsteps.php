b54418ff1e21f09642ccbf8ab21cbfc8926c9f46
hitsteps <hitsteps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-28 06:22:37 +0000

    * update plugin name
    git-svn-id: https://plugins.svn.wordpress.org/hitsteps-visitor-manager/tags/2.33@1015094 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitsteps.php
+++ hitsteps.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Hitsteps Analytics
+Plugin Name: Hitsteps  Real-time Analytics
 Plugin URI: http://www.hitsteps.com/
 Description: Hitsteps is a powerful real time website visitor manager, it allow you to view and interact with your visitors in real time.
 Author: hitsteps
