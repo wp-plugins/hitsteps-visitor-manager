4f2f4195ce3341cf3fd39ddf1339454b3a566886
hitsteps <hitsteps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-08 13:50:59 +0000

    * minor bug fix
    git-svn-id: https://plugins.svn.wordpress.org/hitsteps-visitor-manager/tags/3.08@1063114 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitsteps.php
+++ hitsteps.php
@@ -318,7 +318,7 @@ $postaction='';
 	if ( $option['code']=='' && $postaction!='do' && $_REQUEST['hitmagic']!='do' ) {
 		function hitsteps_warning() {
 			echo "
-			<div id='hitsteps-warning' class='updated fade'><p><strong>".__('hitsteps is almost ready.')."</strong> ".sprintf(__('You must <a href="%1$s">enter your hitsteps API key</a> to start tracking your stats.'), "options-general.php?page=hitsteps-ultimate-web-analytics/hitsteps.php")."</p></div>
+			<div id='hitsteps-warning' class='updated fade'><p><strong>".__('hitsteps is almost ready.')."</strong> ".sprintf(__('You must <a href="%1$s">enter your hitsteps API key</a> to start tracking your stats.'), "options-general.php?page=hitsteps-visitor-manager/hitsteps.php")."</p></div>
 			";
 
 		}
@@ -1705,8 +1705,8 @@ add_action('widgets_init', create_function('', 'return register_widget("hst_STAT
 	# add "Settings" link to plugin on plugins page
 	add_filter('plugin_action_links', 'hitsteps_settingsLink', 0, 2);
 	function hitsteps_settingsLink($actionLinks, $file) {
- 		if (($file == 'hitsteps-ultimate-web-analytics/hitsteps.php') && function_exists('admin_url')) {
-			$settingsLink = '<a href="' . admin_url('options-general.php?page=hitsteps-ultimate-web-analytics/hitsteps.php') . '">' . __('Settings') . '</a>';
+ 		if (($file == 'hitsteps-visitor-manager/hitsteps.php') && function_exists('admin_url')) {
+			$settingsLink = '<a href="' . admin_url('options-general.php?page=hitsteps-visitor-manager/hitsteps.php') . '">' . __('Settings') . '</a>';
 
 			# Add 'Settings' link to plugin's action links
 			array_unshift($actionLinks, $settingsLink);
