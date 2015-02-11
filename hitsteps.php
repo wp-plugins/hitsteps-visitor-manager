4d2d47a8796d31e7c02ff22e003d42d2324ec846
hitsteps <hitsteps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-11 15:31:56 +0000

    * hitsteps update on whitelabel widgets
    git-svn-id: https://plugins.svn.wordpress.org/hitsteps-visitor-manager/trunk@1087194 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitsteps.php
+++ hitsteps.php
@@ -4,7 +4,7 @@ Plugin Name: Hitsteps Ultimate Web Analytics
 Plugin URI: http://www.hitsteps.com/
 Description: Hitsteps is a powerful real time website visitor manager, it allow you to view and interact with your visitors in real time.
 Author: hitsteps
-Version: 3.22
+Version: 3.23
 Author URI: http://www.hitsteps.com/
 */ 
 
