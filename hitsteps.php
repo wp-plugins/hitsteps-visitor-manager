36c5bc1470e6da17b3ea54dfcb689f03704618d4
hitsteps <hitsteps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 12:59:43 +0000

    *Hitsteps ping.php fix for minified script
    git-svn-id: https://plugins.svn.wordpress.org/hitsteps-visitor-manager/trunk@1197935 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitsteps.php
+++ hitsteps.php
@@ -4,7 +4,7 @@ Plugin Name: Hitsteps Ultimate Web Analytics
 Plugin URI: https://www.hitsteps.com/
 Description: Hitsteps is a powerful real time website visitor manager, it allow you to view and interact with your visitors in real time.
 Author: hitsteps
-Version: 4.43
+Version: 4.44
 Author URI: http://www.hitsteps.com/
 */ 
 
