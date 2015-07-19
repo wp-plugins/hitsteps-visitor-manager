ee07cfe8a3943bef9555408367f0dbcbe5d2ad98
hitsteps <hitsteps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-19 11:22:32 +0000

    *update regarding minor bug reported by #30937
    git-svn-id: https://plugins.svn.wordpress.org/hitsteps-visitor-manager/tags/4.45@1201890 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ api.payload.php
@@ -0,0 +1,282 @@
+<?php
+
+$hitsteps_public_web_api_receiver="http://www.hitsteps.com/api/query.php";
+
+if (!function_exists("hitsteps_public_query")){
+	function hitsteps_public_query($post){
+		global $hitsteps_public_web_api_receiver;
+		
+		$hs_option=get_hst_conf();
+		
+		if ($hs_option['code']!=''){
+		
+		
+		$arg=array(
+		'method'=>'POST',
+		'timeout'=>18,
+		'redirection'=>5,
+		'body'=>$post		
+		);
+
+		 //Set the URL to work with
+		$result=wp_remote_post($hitsteps_public_web_api_receiver."?code=".$hs_option['code'],$arg);
+		
+		$arr=array();
+		
+		if ( is_wp_error( $result ) )
+		{
+		$arr['error']=98;
+		$arr['msg']="HTTP_API Call connection error: ".$result->get_error_message();
+
+		return $arr;	
+		}
+
+		if ($result['body']=='db_down_for_maintaince'){
+		$arr['error']=99;
+		$arr['msg']="Hitstep's internal database error";
+
+		return $arr;
+		}
+		if (strpos(strtolower($result['body']),"cloudflare")){
+		$arr['error']=999;
+		$arr['msg']="Hitstep's webserver down at moment. connection couldn't established.";
+
+		return $arr;
+		}
+		
+ 
+
+		
+		$arr=(array) json_decode($result['body'], true);
+		$arr['completed']=1;
+		
+		return $arr;
+		
+		}else{
+		$arr['error']=120;
+		$arr['msg']="You need to install hitsteps API code in setting menu.";
+		return $arr;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+function _hs_contact_form_query_plain($input){
+$render='';
+$input['action']='contact_form_data_1';
+
+//visitor data
+$input['user_ip']=_hitsteps_get_ip();
+$input['user_agent']=$_SERVER['HTTP_USER_AGENT'];
+
+$data=hitsteps_public_query($input);
+$hs_option=get_hst_conf();
+if ($hs_option['code']!=''){
+if ($data['error']>0){
+$render="Hitsteps error #".$data['error'].": ".$data['msg']."";
+}else{
+
+$render.="\n--- Hitsteps Analytics ---\n";
+
+
+if ($input['output_visitor_ip']){
+if (is_array($data['visitor_ip'])){
+$devicetype="PC";
+if ($data['visitor_ip']['mobile']) $devicetype="Phone";
+if ($data['visitor_ip']['tablet']) $devicetype="Tablet";
+
+$render.="\nName: ".$data['visitor_ip']['name']."\nFrom: ".$data['visitor_ip']['city'].", ".$data['visitor_ip']['country_name']."\nOrg/ISP: ".$data['visitor_ip']['org']."\nDevice: ".$data['visitor_ip']['sw']."x".$data['visitor_ip']['sh']." ".$data['visitor_ip']['browser']." ".$data['visitor_ip']['browserv']." - ".$data['visitor_ip']['os']." - ".$devicetype." IP: ".$data['visitor_ip']['ip']."\n";
+
+}
+}
+
+
+
+if ($input['output_visitor_base']){
+if (is_array($data['visitor_base'])){
+
+$render.="
+\nFirst visited ".$data['visitor_base']['base']." from ". $data['visitor_base']['baseref'] ." landed on ".$data['visitor_base']['baseland']."\n";
+}
+}
+
+
+
+if ($input['output_visitor_path']){
+if (is_array($data['visitor_path'])){
+
+$totalview=$data['visitor_path']['hits'];
+$more='';
+if ($totalview>50){$totalview="more than 50";
+$more='more than ';
+}
+
+$render.="\nRecent pageviews are ".$totalview." pages, result in spending ".$more.round($data['visitor_path']['avg']/60,0)." minutes:\n";
+
+
+if (is_array($data['visitor_path']['pages'])){
+
+$count=20;
+
+foreach ($data['visitor_path']['pages'] as $pg){
+$count--;
+if ($count>0){
+$pg['refname']=$pg['ref'];
+$pg['reflink']="href='".$pg['ref']."'";
+if ($pg['ref']==''){$pg['refname']='Direct';$pg['reflink']='';}
+
+
+$render.="\nTo: ".$pg['name']." - visited ".$pg['short_date']."\nFrom: ".$pg['refname']."";
+
+}}
+
+}
+
+$render.="\n";
+}
+}
+
+
+if ($input['output_visitor_link']){
+if (isset($data['visitor_link'])){
+
+$render.="\nKnow more about this visitor: ".$data['visitor_link'];
+}
+}
+
+
+$render.="\n";
+
+
+}
+}
+
+
+return $render;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function _hs_contact_form_query($input){
+$render='';
+$input['action']='contact_form_data_1';
+
+//visitor data
+$input['user_ip']=_hitsteps_get_ip();
+$input['user_agent']=$_SERVER['HTTP_USER_AGENT'];
+
+$data=hitsteps_public_query($input);
+$hs_option=get_hst_conf();
+if ($hs_option['code']!=''){
+if ($data['error']>0){
+$render="<small><i>Hitsteps error #".$data['error'].": ".$data['msg']."</i></small>";
+}else{
+
+$render.="<!--Hitsteps: You can hide this column --><div style='margin:15px;padding:5px;padding-bottom:0px;background: #23282D; border:1px solid #eee; font-family: Helvetica, arial, tahoma; font-size: 9pt;'><img src='" . WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)). "logo.png' width='90' height='20' alt='hitsteps analytics' />";
+
+
+if ($input['output_visitor_ip']){
+if (is_array($data['visitor_ip'])){
+$devicetype="PC";
+if ($data['visitor_ip']['mobile']) $devicetype="Phone";
+if ($data['visitor_ip']['tablet']) $devicetype="Tablet";
+
+$render.="<div style='padding:10px;margin-bottom:5px;background: #efefef; '><img style='float: left; margin-right: 5px; margin-bottom:5px;border: #eee 1px solid;' src='http://www.hitsteps.com/responsive/flags/".strtoupper($data['visitor_ip']['country']).".png' width='74' height='50'>Name: ".$data['visitor_ip']['name']."<br>From: ".$data['visitor_ip']['city'].", ".$data['visitor_ip']['country_name']."<br>Org/ISP: ".$data['visitor_ip']['org']."<br>Device: ".$data['visitor_ip']['sw']."x".$data['visitor_ip']['sh']." ".$data['visitor_ip']['browser']." ".$data['visitor_ip']['browserv']." - ".$data['visitor_ip']['os']." - ".$devicetype." IP: ".$data['visitor_ip']['ip']."</div>";
+
+}
+}
+
+
+
+if ($input['output_visitor_base']){
+if (is_array($data['visitor_base'])){
+
+$render.="
+<div style='padding:10px;margin-bottom:5px;background: #efefef; '>First visited ".$data['visitor_base']['base']." from ". $data['visitor_base']['baseref'] ." landed on ".$data['visitor_base']['baseland']."</div>";
+}
+}
+
+
+
+if ($input['output_visitor_path']){
+if (is_array($data['visitor_path'])){
+
+$totalview=$data['visitor_path']['hits'];
+$more='';
+if ($totalview>50){$totalview="more than 50";
+$more='more than ';
+}
+
+$render.="<div style='padding:10px;margin-bottom:5px;background: #efefef; '>Recent pageviews are ".$totalview." pages, result in spending ".$more.round($data['visitor_path']['avg']/60,0)." minutes:<br><br>";
+
+
+if (is_array($data['visitor_path']['pages'])){
+
+$count=20;
+
+foreach ($data['visitor_path']['pages'] as $pg){
+$count--;
+if ($count>0){
+$pg['refname']=$pg['ref'];
+$pg['reflink']="href='".$pg['ref']."'";
+if ($pg['ref']==''){$pg['refname']='Direct';$pg['reflink']='';}
+
+
+$render.="<div style='padding:5px; margin: 3px;margin-bottom:5px;background: #fff; line-height: 15pt;'>To: <a style='text-decoration: none;color: #666;' href='".$pg['url']."'>".$pg['name']."</a> - visited ".$pg['short_date']."<br><i style='text-decoration: none;color: #999;'>From: <a style='text-decoration: none;color: #999;' ".$pg['reflink'].">".$pg['refname']."</a></i></div>";
+
+}}
+
+}
+
+$render.="</div>";
+}
+}
+
+
+if ($input['output_visitor_link']){
+if (isset($data['visitor_link'])){
+
+$render.="<a style='display:block; padding:10px; background: #23282D;color: #fff; text-decoration: none;' href='".$data['visitor_link']."'>Know more about this visitor</a>";
+}
+}
+
+
+$render.="</div>";
+
+
+}
+}
+
+
+return $render;
+}
+
+
+	 function _hitsteps_get_ip()
+    {
+			$ip=$_SERVER["HTTP_CF_CONNECTING_IP"];
+			if ($ip=='') {$ip=$_SERVER["HTTP_X_FORWARDED"];}
+			if ($ip=='') {$ip=$_SERVER["HTTP_FORWARDED_FOR"];}
+			if ($ip=='') {$ip=$_SERVER["HTTP_CLIENT_IP"];}
+			if ($ip=='') {$ip=$_SERVER["REMOTE_ADDR"];}
+			return $ip;
+	}
+
+?>
